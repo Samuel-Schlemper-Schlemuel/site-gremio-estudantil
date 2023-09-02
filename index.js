d9b125057d75069c57a193547cc46f10144c8e5f
Samuel Schlemper <samuelschlemper2006@gmail.com>
2023-09-01 22:15:06 -0300

    Acho que descobri o erro

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/imagens'))
 app.use(express.static(__dirname + '/imagens/integrantes'))
 app.use(express.static(__dirname + '/js'))
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/ejs')
+app.set('views', __dirname + '/EJS')
 
 app.use(express.urlencoded({
     extended: true
